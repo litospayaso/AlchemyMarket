ded15935e2c271344c278f98d3ae9d9fc694a2fd
Hita <697729@cognizant.com>
2019-04-25 13:31:15 +0200

    adding npm start server

--- src/server.js
+++ src/server.js
@@ -3,3 +3,4 @@ const Server = require("boardgame.io/server").Server;
 const TicTacToe = require("./game").TicTacToe;
 const server = Server({ games: [TicTacToe] });
 server.run(8000);
+console.log('\x1b[32m%s\x1b[37m%s\x1b[0m', 'Server.js: ', `server listening on http://localhost:8000`);
